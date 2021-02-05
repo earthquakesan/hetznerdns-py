e0b6a08e695dbcaaf39e206126bb87c8f0d29ae8
Ivan Ermilov <earthquakesan@gmail.com>
2021-02-05 14:22:10 +0100

    bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hetznerdns",
-    version="1.0.0",
+    version="1.0.1",
     author="Ivan Ermilov",
     author_email="ivan.s.ermilov@gmail.com",
     description="An API and CLI client for Hetzner DNS service.",
