f1c52bb7b7f376fb63cfb3b975384d70bd218ded
Ivan Ermilov <earthquakesan@gmail.com>
2021-02-05 13:51:27 +0100

    move hetznerdns-cli.py to scripts/hetznerdns-cli, add to setup.py as a script

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="",
     packages=setuptools.find_packages(),
+    scripts=['scripts/hetznerdns-cli'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
